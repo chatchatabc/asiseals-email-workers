4ff34f1b0d250169c983a2cbd3c2886a7a852c25
BoJoNVi <bonjomontes@gmail.com>
2023-04-13 14:48:44 +0800

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { EmailMessage } from "cloudflare:email";
+import { createMimeMessage } from "mimetext";
+
+export interface Env {
+  SEB: SendEmail;
+  KV: KVNamespace;
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const msg = createMimeMessage();
+
+    msg.setSender({ name: "GPT-4", addr: "bon@bonkind.xyz" });
+    msg.setRecipient("bonjovimontes3@gmail.com");
+    msg.setSubject("An email generated in a worker");
+    msg.addMessage({ contentType: "text/plain", data: "Hello, world!" });
+
+    var message = new EmailMessage(
+      "bon@bonkind.xyz",
+      "bonjovimontes3@gmail.com",
+      msg.asRaw()
+    );
+    try {
+      await env.SEB.send(message);
+    } catch (e: any) {
+      return new Response(e);
+    }
+
+    return new Response("Hello Send Email World!");
+  },
+};
